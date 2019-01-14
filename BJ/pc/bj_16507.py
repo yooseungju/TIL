88257e9f324dac096a6727002e078d88857dc88c
yooseungju <seung338989@gmail.com>
2019-01-15 07:59:54 +0900

    Bj  16507

--- BJ/pc/bj_16507.py
+++ BJ/pc/bj_16507.py
@@ -1,38 +1,44 @@
 r, c, num = map(int, input().split())
 matrix = []
-for _ in range(5):
+for _ in range(r):
     matrix.append((list(map(int, input().split()))))
 
 
-print(matrix)
-
-hab = '0'
-
+hab = 0
+#부분합 구하는 부분
 for i in range(r):
-    for j in range(0 , c):
-        if i == 0:
-            if j != 0:
-                matrix[i][j] += matrix[i][j-1]
+    for j in range(1 ,c):
+        matrix[i][j] += matrix[i][j-1]
 
-
-        elif i > 0 and j > 0:
-            matrix[i][j] += matrix[i-1][j-1]
-
-        else:
-            matrix[i][j] += matrix[i-1][j]
-
-
-    
+for i in range(1,r):
+    for j in range(0, c):
+        matrix[i][j] += matrix[i-1][j]
 
 
 
+for i in range(num):
+    ax, ay, bx, by = map(int, input().split())
+
+    ax, ay, bx, by = ax-1, ay-1, bx-1, by-1
+
+    print(i+1 , ': ',ax, ay, bx, by)
 
 
 
+    if ay == 0 and ax == 0:
+        hab = matrix[by][bx]
+    elif ax == 0:
+        hab = matrix[by][bx] - matrix[ay - 1][bx]
+
+    elif ay == 0:
+        hab = matrix[by][bx] - matrix[by][ax - 1]
+
+    else:
+        hab = matrix[by][bx] - matrix[ay-1][bx] - matrix[by][ax-1] + matrix[ay-1][ax-1]
 
 
+    print(hab , "dfdfa")
 
-print(matrix)
 
 
 
