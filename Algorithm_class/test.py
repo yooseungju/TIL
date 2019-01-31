346eb08740e577f4253297ee34e70bbb4a00d888
yooseungju <seung338989@gmail.com>
2019-01-31 17:47:19 +0900

    D16 | 190131 | Flask and SQL

--- Algorithm_class/test.py
+++ Algorithm_class/test.py
@@ -1,8 +1,50 @@
-import datetime
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
-dt1 = datetime.datetime.now()
-dt2 = datetime.datetime(2019, 5, 30)
-td = dt2 - dt1
+class Circle:
+    def __init__(self, center, r):
+        self.center = center
+        self.r = r
 
-print(td)
+    def get_area(self):
+        return 3.14*self.r**2
+
+    def get_perimeter(self):
+        return 3.14*2*self.r
+
+    def get_center(self):
+        return (self.center.x, self.center.y)
+
+    def print(self):
+        print(f"Circle: {self.get_center()}, r: {self.r}" )
+
+
+
+
+p1 = Point(0,0)
+c1 = Circle(p1, 3)
+print(c1.get_area())
+print(c1.get_perimeter())
+print(c1.get_center())
+c1.print()
+
+
+p2 = Point(4,5)
+c2 = Circle(p2,1)
+print(c2.get_area())
+print(c2.get_perimeter())
+print(c2.get_center())
+c2.print()
+
+
+def F(c, a=1, b=3):
+    print(a,b,c)
+
+F(1,2,3)
+F(1,2)
+F(1)
+
+
