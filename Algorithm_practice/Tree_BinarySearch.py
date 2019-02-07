9f1ea6d7b7abb125bac8366eec722fd981cac243
yooseungju <seung338989@gmail.com>
2019-02-08 01:34:11 +0900

    algorithm

--- Algorithm_practice/Tree_BinarySearch.py
+++ Algorithm_practice/Tree_BinarySearch.py
@@ -1,8 +1,42 @@
 import sys
 sys.stdin = open("input.txt")
 
+def binarysearch(num):
+    n = 1
+    k = 1
+    for i in range(9):
+        if num >= (2**i)-1 and num < 2**(i+1)-1:
+            n += (2**(i-1))-1
+            if num <= ((2**(i+1)-1)+((2**i)-1))//2:
+                n += num - ((2**i)-1)
+            else:
+                n += 2**(i-1)
+            break
+
+    for i in range(9):
+        if num >= (2**i)-1 and num < 2**(i+1)-1:
+            if num == (2**i)-1:
+                k = num
+                break
+            else:
+                k = (num - ((2 ** i) - 1))*2 -1
+                break
+
+
+    if num%2:
+        k -= 1
+    else:
+        k += 1
+
+
+    return n, k
+
+
 
 T = int(input())
 
+
 for tc in range(T):
-    print(f"#{tc+1} {}")
+    num = int(input())
+    n, k =binarysearch(num)
+    print(f"#{tc+1} {n} {k}")
