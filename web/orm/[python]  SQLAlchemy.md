2764332d1b10fb80d4a9dd18c40f5590d6ee876f
yooseungju <seung338989@gmail.com>
2019-02-07 17:41:05 +0900

    D18| 190207 | orm CRUD

---
+++ web/orm/[python]  SQLAlchemy.md
@@ -0,0 +1,31 @@
+ORM의 장점
+
+1. 객체지향적인 코드로 인해 직관적이고 비즈니스 로직에 더 집중할 수 있게한다.
+2. 재사용/ 유지보수가 증가
+3. DB에 대한 종속성이 줄어듦
+
+ORM의 단점
+
+1. 오로지 ORM으로만은 거대한 서비스를 구현하기가 어렵다
+2. 어느정도의 속도 저하
+
+### [python]  SQLAlchemy
+
+1. sqlalchemy 설치하기
+
+   ```python
+   pip install sqlalchemy
+   ```
+
+   
+
+
+
+CREATE
+
+READ
+
+DELETE
+
+UPDATE
+
---
+++ web/orm/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_migrate import Migrate
+from models import db, User
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+# flask app 에 sqlalchemy 관련설정
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db_flask.sqlite3"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
+# sqlalchemy데이터 베이스 객체 수정 및 신호 발송들을 추적 , 과도한 메모리를 사용하기 때문애 False
+
+# sqlalchemy초기화
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users = users)
+    
+@app.route('/users/create')
+def create():
+    username = request.args.get('username')
+    email = request.args.get('email')
+    user = User(username = username, email = email)
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+    
+    
+@app.route('/users/delete/<int:id>')
+def delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/users/edit/<int:id>')
+def edit(id):
+    user = User.query.get(id)
+    return render_template('edit.html', user = user)
+    
+    
+@app.route('/users/update/<int:id>')
+def update(id):
+    user = User.query.get(id)
+    username = request.args.get('username')
+    email = request.args.get('email')
+    user.username = username
+    user.email = email
+    db.session.commit()
+    return redirect(url_for('index'))
+    
+    
+    
+
+if __name__ == '__main__':
+    app.run(host = os.getenv('IP'), port = os.getenv('PORT'), debug = True)
