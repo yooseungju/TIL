8dd9451b6e15665403b8207664d896aebed5c655
yooseungju <seung338989@gmail.com>
2019-02-21 17:53:41 +0900

    D21 | 190221 | django CRUD02

--- Algorithm_practice/SWexpert1767.py
+++ Algorithm_practice/SWexpert1767.py
@@ -3,12 +3,47 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 
-def ans():
-    num = int(input())
+di = [0,0,0,0]
 
-    matrix = [list(map(int, input().split())) for _ in range(num)]
+
+
+def backtraking(sum, n, k):
+    global min
+    global cores
+    global matrix
+    global visited
+
+    di = [0,0,-1,1]
+    dj = [-1,1,0,0]
+
+    for i in range(1,n):
+        for j in range(1,n):
+            if matrix[i][j] == 1:
+                for m in range(4):
+                    if matrix[i+di[m]][j+dj[m]] == 0:
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 for tc in range(T):
-    print(f'{tc+1} {ans()}')
+    num = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(num)]
+    sum = 0
+    k = 0
+    min = 100
+    cores = []
+    visited = [0] * len(cores)
+
+
+
+    print(f'{tc+1} {min}')
---
+++ Algorithm_practice/test.py
@@ -0,0 +1,10 @@
+def ans(a):
+
+
+
+def main():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    ans(a)
+
+if __name__ == '__main__':
+    main()
