08a632b5a95a4129bd7c1dd2772119ac7b11a2ae
yooseungju <seung338989@gmail.com>
2019-04-17 23:14:10 +0900

    home

---
+++ Algorithm_class02/BJ/[17143]낚시왕.py
@@ -0,0 +1,90 @@
+import sys
+sys.stdin = open('input.txt')
+
+def change(x, y, d):
+    dj = [0, 0, 0, 1, -1]
+    di = [0, -1, 1, 0, 0]
+
+    nx = x + di[d]*s
+    ny = y + dj[d]*s
+
+    while True:
+        if 0 <= nx < R and 0 <= ny < C:
+            return nx, ny, d
+
+        #우 초과
+        if nx > (R - 1):
+            nx = (R - 1) - (nx - (R - 1))
+            d = 4
+
+        #좌 초과
+        if nx < 0:
+            nx =  abs(nx)
+            d = 3
+
+        #하 초과
+        if ny > (C - 1):
+            ny = (C - 1) - (ny - (C - 1))
+            d = 1
+
+        # 상 초과
+        if ny < 0:
+            d = 2
+            ny = abs(ny)
+
+
+
+def move():
+    # 1 위 , 2  아래,  3  오른쪽 , 4 왼쪽
+    dj = [0,0,0,1,-1]
+    di = [0,-1,1,0,0]
+
+    for x in range(R):
+        for y in range(C):
+            if G[x][y] != 0:
+                s, d, z = G[x][y]
+
+                nx, ny, d = change(x,y,d)
+
+                if visited[nx][ny]  ==  0:
+                    visited[nx][ny] = (s,d,z)
+
+                else:
+                    s1, d1, z1 = visited[nx][ny]
+
+                    if z1 < z:
+                        visited[nx][ny] = (s,d,z)
+
+
+
+R , C,  M = map(int, input().split())
+
+G = [[0]*C for _ in range(R)]
+
+
+
+for _ in range(M):
+    r, c ,s, d, z = map(int, input().split())
+    G[r-1][c-1] = (s,d,z)
+
+count = 0
+
+
+
+for f_k in range(C):
+    visited = [[0] * C for _ in range(R)]
+    for i in range(R):
+        if G[i][f_k]  !=  0:
+            s,d,z = G[i][f_k]
+            count += z
+            G[i][f_k] = 0
+            break
+
+    move()
+
+    for i in range(R):
+        for j in range(C):
+            G[i][j] = visited[i][j]
+
+
+print(count)
--- Algorithm_class02/BJ/[17144]미세먼지 안녕.py
+++ Algorithm_class02/BJ/[17144]미세먼지 안녕.py
@@ -4,18 +4,15 @@ sys.stdin = open('input.txt')
 
 
 def move():
-
     uj = [1,0,-1,0]
     ui = [0,-1,0,1]
-
-    dummy = [(uX,C-1),(0,C-1),(0,0),(uX,uY)]
-
+    dummy = [(uX,C-1),(0,C-1),(0,0)]
     x = uX
     y = uY
 
     pre = 0
     c = 0
-    while c < 4:
+    while True:
         x += ui[c]
         y += uj[c]
         if x==uX and y==uY:
@@ -30,20 +27,15 @@ def move():
             c += 1
 
 
-
-
     dj = [1, 0, -1, 0]
     di = [0, 1, 0, -1]
-
-    dummy = [(dX, C-1), (R-1, C-1), (R-1, 0), (uX, uY)]
-
+    dummy = [(dX, C-1), (R-1, C-1), (R-1, 0)]
     x = uX + 1
     y = uY
-
     pre = 0
     c = 0
 
-    while c < 4:
+    while True:
         x += di[c]
         y += dj[c]
         if x==dX and y==dY:
@@ -56,91 +48,67 @@ def move():
 
         if (x,y) in dummy:
             c += 1
+    #
+    # for i in range(R):
+    #     for j in range(C):
+    #         print(M[i][j], end=' ')
+    #
+    #     print()
+    # print()
 
-    for i in range(R):
-        for j in range(C):
-            print(M[i][j], end=' ')
-        print()
-    print()
-
-
-
-
-
-
-
-
-def BFS(Q):
-    visited = [[0] * C for _ in range(R)]
-
-    cnt = 0
-    dj = [-1,1,0,0]
-    di = [0,0,-1,1]
-
-    while len(Q) > 0:
-        x,y = Q.pop(0)
-
-        if x == 1001:
-
-            for i in range(R):
-                for j in range(C):
-                    M[i][j] += visited[i][j]
-
-            move()
-
-            Q.append((1001,1001))
-            cnt += 1
-            visited = [[0] * C for _ in range(R)]
-
-
-
-
-            if cnt == T:
-
-                return
-            continue
-        n = 0
-        for m in range(4):
-
-            nx = x + di[m]
-            ny = y + dj[m]
-
-            if 0<=nx<R and 0<=ny<C:
-                if M[nx][ny] != -1:
-                    n += 1
-                    e = M[x][y]//5
-                    visited[nx][ny] += e
-
-
-        if n > 0:
-            M[x][y] -= (M[x][y]//5)*n
-
+    return
 
 
 R, C, T = map(int, input().split())
 M = [list(map(int, input().split())) for _ in range(R)]
 clear = []
-dust = []
 
 for i in range(R):
     for j in range(C):
         if M[i][j] == -1:
             clear.append((i,j))
-        elif M[i][j] != 0:
-            dust.append((i,j))
 
 
 uX, uY = clear[0]
 dX, dY = clear[1]
 
-dust.append((1001,1001))
-BFS(dust)
+
+
+dj = [-1,1,0,0]
+di = [0,0,-1,1]
+
+while  T>0:
+    visited = [[0] * C for _ in range(R)]
+
+    for i in range(R):
+        for j in range(C):
+            if M[i][j] > 0:
+                n = 0
+                for m in range(4):
+                    nx = i + di[m]
+                    ny = j + dj[m]
+
+                    if 0 <= nx < R and 0 <= ny < C:
+                        if M[nx][ny] != -1:
+                            e = M[i][j] // 5
+                            visited[nx][ny] += e
+                            n+=1
+                if n > 0:
+                    M[i][j] -= (M[i][j]//5)*n
+
+    for i in range(R):
+        for j in range(C):
+            if visited[i][j] > 0:
+                M[i][j] += visited[i][j]
+
+    move()
+    T-=1
+
+
 SUM = 0
-
-
 for i in range(R):
     for j in range(C):
-        if M[i][j] != -1:
+        if M[i][j] > 0:
             SUM += M[i][j]
 
 print(SUM)
