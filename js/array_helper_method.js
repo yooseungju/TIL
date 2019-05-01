8b0913e99b9458f6997bb7e600eb1af47238fde2
yooseungju <seung338989@gmail.com>
2019-05-01 17:55:18 +0900

    0501 | javascript

---
+++ js/array_helper_method.js
@@ -0,0 +1,18 @@
+const NUMBERS = [1,2,3,]
+
+const DOUBLE_NUMBERS = NUMBERS.map(function(number){
+    return number * 2
+})
+
+// const DOUBLE_NUMBERS = NUMBERS.map(number => number*2)
+
+console.log(DOUBLE_NUMBERS)
+
+
+const images  = [
+   { height: '34px', width:'39px'},
+   { height: '54px', width:'19px'},
+   { height: '83px', width:'95px'},
+
+]
+const hights = 
