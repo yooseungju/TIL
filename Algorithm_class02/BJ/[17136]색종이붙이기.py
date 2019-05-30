4976a536f4bbc2f2d0738bf72e5e84eb4d278fba
yooseungju <seungju338989@gmail.com>
2019-05-30 17:55:11 +0900

    0530 | java | algorithm

--- Algorithm_class02/BJ/[17136]색종이붙이기.py
+++ Algorithm_class02/BJ/[17136]색종이붙이기.py
@@ -1,68 +1,62 @@
 import sys
 
 sys.stdin = open('input.txt')
-SIZE = 10
 
 
 def check(x,y,k):
-    flag = True
     for i in range(k):
         for j in range(k):
-            nx = x+i
-            ny = y+j
-            if not 0<=nx<SIZE or not 0<=ny<SIZE or M[nx][ny] == 0:
-                flag = False
-    return flag
-
+            if x+i > 9 or y+j> 9 or M[x+i][y+j] == 0:
+                return False
+    return True
 
 def change(x,y,k,v):
     for i in range(k):
         for j in range(k):
-            M[i + x][j + y] = v
+            M[i+x][j+y] = v
 
+def search(c):
+    global MIN
 
-def search(k, count, result, SUM):
-    global Min
-
-    if(k==0):
-        if C - SUM != 0:
-            return
-        if result < Min:
-            Min = result
+    if c > MIN:
         return
 
-    flag = 1
+    count = 0
 
     for i in range(SIZE):
-        if flag:
-            for j in range(SIZE):
-                if M[i][j] == 1 and check(i,j,k):
-                    if count < 5:
-                        change(i,j,k,0)
-                        search(k, count+1, result, SUM)
+        for j in range(SIZE):
+            count += 1
+            if M[i][j] == 1:
+                for k in range(5,0,-1):
+                    if chk[k] > 0 and check(i,j,k):
+                        change(i, j, k, 0)
+                        chk[k] -= 1
+
+                        search(c+1)
+
                         change(i, j, k, 1)
+                        chk[k] += 1
+                return
 
-                    flag = 0
-                    break
-        else:
-            break
 
-    search(k - 1, 0, result + count, SUM + (k ** 2) * count)
+    if c < MIN:
+        MIN = c
+    return
+
+
 
 
 #main----------------------------------------------------------
-
-Min = 0xfffffff
+SIZE = 10
 M = [list(map(int, input().split())) for _ in range(SIZE)]
 C = 0
-for i in range(SIZE):
-    for j in range(SIZE):
-        if M[i][j] == 1:
-            C += 1
 
-search(5, 0, 0, 0)
+chk = [5]*6
+MIN = 0xfffffff
 
-if Min == 0xfffffff:
+search(0)
+
+if MIN == 0xfffffff:
     print(-1)
 else:
-    print(Min)
+    print(MIN)
